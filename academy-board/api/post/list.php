66100c8c8ddc62a3e3ad41adc2449f505c10fbb7
unknown <riverchoi2523@gmail.com>
2025-10-16 11:46:46 +0900

    fix: list.php - 작성자

--- academy-board/api/post/list.php
+++ academy-board/api/post/list.php
@@ -1,11 +1,21 @@
 <?php
-//header('Content-Type: application/json; charset=utf-8');
+header('Content-Type: application/json; charset=utf-8');
 require_once '../../config/database.php';
 
 try {
   $boardType = $_GET['type'] ?? 'notice';
 
-  $sql = "SELECT id, title, created_at FROM posts WHERE board_type = :board_type ORDER BY created_at DESC";
+  // posts와 users 테이블 조인
+  $sql = "SELECT 
+            p.id, 
+            p.title, 
+            p.created_at,
+            u.nickname AS author_nickname
+          FROM posts p
+          LEFT JOIN users u ON p.user_id = u.id
+          WHERE p.board_type = :board_type 
+          ORDER BY p.created_at DESC";
+  
   $stmt = $pdo->prepare($sql);
   $stmt->execute(['board_type' => $boardType]);
 
